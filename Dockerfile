c4cf9de2d06fbd0479306762643ff9d0f926fa08
571530 <truls.martinsen13@gmail.com>
2019-10-03 09:58:42 +0200

    test

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@
 FROM node:10
 
 # Copies your code file from your action repository to the filesystem path `/` of the container
-COPY deploy.sh /deploy.sh
+add deploy.sh /deploy.sh
 
 # Code file to execute when the docker container starts up (`entrypoint.sh`)
 ENTRYPOINT ["/deploy.sh"]
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,38 @@
 #!/bin/sh -l
 
 echo "hei ${GITHUB_ACTOR}"
+
+apt-get update && \
+apt-get install -y git && \
+apt-get install -y jq
+
+COMMIT_EMAIL=`jq '.pusher.email' ${GITHUB_EVENT_PATH}`
+COMMIT_NAME=`jq '.pusher.name' ${GITHUB_EVENT_PATH}`
+
+git init && \
+git config --global user.email "${COMMIT_EMAIL}" && \
+git config --global user.name "${COMMIT_NAME}"
+
+REPOSITORY_PATH="https://${ACCESS_TOKEN:-"x-access-token:$GITHUB_TOKEN"}@github.com/${GITHUB_REPOSITORY}.git"
+
+if [ "$(git ls-remote --heads "$REPOSITORY_PATH" "$BRANCH" | wc -l)" -eq 0 ];
+then
+  echo "Creating remote branch ${BRANCH} as it doesn't exist..."
+  git checkout "${BASE_BRANCH:-master}" && \
+  git checkout --orphan $BRANCH && \
+  git rm -rf . && \
+  touch README.md && \
+  git add README.md && \
+  git commit -m "Initial ${BRANCH} commit" && \
+  git push $REPOSITORY_PATH $BRANCH
+fi
+
+git checkout "${BASE_BRANCH:-master}"
+
+echo "Deploying to GitHub..."
+git add -f $FOLDER
+
+git commit -m "Deploying to ${BRANCH} from ${BASE_BRANCH:-master} ${GITHUB_SHA}" --quiet && \
+git push $REPOSITORY_PATH `git subtree split --prefix $FOLDER ${BASE_BRANCH:-master}`:$BRANCH --force && \
+
+echo "Deployment succesful!"
